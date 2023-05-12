eea8dd1f621981e570169ac46726f9ff824180f5
Argiro <elto.82@gmai.com>
2023-05-11 20:23:57 -0500

    deploy

---
+++ src/db.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from "sequelize-typescript";
+import config from "../lib/config";
+config;
+export const sequelize = new Sequelize({
+  dialect: "postgres",
+  database: config.dbName,
+  password: config.dbPassword,
+  username: config.dbUser,
+  port: Number(config.dbPort),
+  storage: ":memory:",
+  models: [__dirname + "/models"],
+  logging: false,
+});
