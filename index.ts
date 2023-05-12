f1261116dfd3a579dd6e817227f96468e69fff0e
Argiro <elto.82@gmai.com>
2023-05-12 01:03:34 -0500

    ok

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { sequelize } from "./src/db";
 require("dotenv").config();
-const PORT = process.env.API_PORT || 3001;
+const PORT = process.env.API_PORT;
 import app from "./src/app";
 
 sequelize
@@ -8,7 +8,7 @@ sequelize
   .then(() => {
     console.log("base de datos conectada! :D");
     app.listen(PORT, function () {
-      console.log("App is listening on port 3001!");
+      console.log("App is listening on port :" + PORT);
     });
   })
   .catch((err) => console.error(err));
