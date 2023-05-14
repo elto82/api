f033fbbaf661118b621b0d1771e1bdac8414176b
Argiro <elto.82@gmai.com>
2023-05-13 23:40:10 -0500

    update cors

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,12 @@ var cors = require("cors");
 const app: Application = express();
 
 app.use(
-  app.use(cors());
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+  })
 );
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" })); //middleware
