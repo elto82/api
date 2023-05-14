fa55f72d537367ae262148d22f0745147bfad6e2
Argiro <elto.82@gmai.com>
2023-05-13 23:33:57 -0500

    cors

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,7 @@ var cors = require("cors");
 const app: Application = express();
 
 app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-    methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
-    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
-  })
+  app.use(cors());
 );
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" })); //middleware
