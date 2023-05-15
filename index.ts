e5947dd2342453bd985c5834437cc1b3d84a02e2
Argiro <elto.82@gmai.com>
2023-05-15 00:03:40 -0500

    pruba de port sin ip

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ sequelize
     console.log("base de datos conectada! :D");
     const port = parseInt(process.env.PORT, 10) || 3000;
 
-    app.listen(port, "0.0.0.0", function () {
+    app.listen(port, async function () {
       console.log(`App is listening on port ${port}! name DB ${NAME}!`);
     });
   })
