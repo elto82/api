24e4e1924b9df05d5402ae24e2d4d7d9cd843a0e
Argiro <elto.82@gmai.com>
2023-05-14 20:22:59 -0500

    print name db

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { sequelize } from "./src/db";
 import config from "./lib/config";
 
 const PORT = config.port;
+const NAME = config.dbName;
 import app from "./src/app";
 
 sequelize
@@ -9,7 +10,7 @@ sequelize
   .then(() => {
     console.log("base de datos conectada! :D");
     app.listen(PORT, function () {
-      console.log("App is listening on port :" + PORT);
+      console.log(`App is listening on port ${PORT}! name DB ${NAME}!`);
     });
   })
   .catch((err) => console.error(err));
