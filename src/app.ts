bc715f11a325758719ec72ff67cf8babb470cc27
Argiro <elto.82@gmai.com>
2023-05-20 16:44:17 -0500

    cors

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,7 @@ var cors = require("cors");
 
 const app: Application = express();
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-    methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
-    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
-  })
-);
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
