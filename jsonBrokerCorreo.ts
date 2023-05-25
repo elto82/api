4f0f2e3d8b874640176bb59bd549f427a21e15d5
Argiro <elto.82@gmai.com>
2023-05-25 00:01:32 -0500

    force: false

--- jsonBrokerCorreo.ts
+++ jsonBrokerCorreo.ts
@@ -1,23 +1,27 @@
 export const countBroker = [
   {
+    id: "712913",
     division: "local",
     email: "proptech.broker.local@gmail.com",
     password: "brokerlocal123",
     name: "david perez",
   },
   {
+    id: "123456",
     division: "vivienda",
     email: "proptech.broker.vivienda@gmail.com",
     password: "brokervivienda123",
     name: "maria jimenez",
   },
   {
+    id: "223456",
     division: "oficina",
     email: "proptech.broker.oficina@gmail.com",
     password: "brokeroficina123",
     name: "manuel morales",
   },
   {
+    id: "333456",
     division: "industria",
     email: "proptech.broker.industria@gmail.com",
     password: "brokerindustria123",
